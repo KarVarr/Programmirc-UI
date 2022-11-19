3775e8abab406471b47f1b1b9ba857ef38f8d24f
KarVarr <megatr9n@gmail.com>
2022-11-19 23:30:42 +0300

    move the button to center  and added  a second screen

--- Programmirc-UI/FirstScreen.swift
+++ Programmirc-UI/FirstScreen.swift
@@ -14,7 +14,7 @@ class FirstScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupButton()
-        view.backgroundColor = .cyan
+        view.backgroundColor = .brown
     }
     
     func setupButton() {
@@ -23,6 +23,17 @@ class FirstScreen: UIViewController {
         nextButton.configuration = .filled()
         nextButton.configuration?.baseBackgroundColor = .systemPink
         nextButton.configuration?.title = "Next"
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: 200),
+            nextButton.heightAnchor.constraint(equalToConstant: 50),
+            
+        ])
+        
     }
 
 }
---
+++ Programmirc-UI/SecondScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SecondScreen.swift
+//  Programmirc-UI
+//
+//  Created by Karen Vardanian on 19.11.2022.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemOrange
+    }
+    
+}
